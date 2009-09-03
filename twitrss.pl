0073edb2d963e1d37e78b4a2cc5cbddabd59cbaf
kujoo <kujoo@wiir.jp>
2009-09-03 21:27:10 +0900

    add twitscrape.pl, twitrss.pl. (twitrss.pl is as contents as twitscrape.pl is

---
+++ twitrss.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use utf8;
+#use CGI;
+#use CGI::Carp qw(fatalsToBrowser);
+use Encode;
+#use open ':utf8';
+#  binmode STDIN, ":utf8";
+#  binmode STDOUT, ":utf8";
+use Config::Pit;
+#use LWP::UserAgent;
+#use MIME::Base64;
+#use HTML::TagParser;
+use Web::Scraper;
+use URI;
+use Data::Dumper;
+
+    my $input = $ARGV[0] or die 'no input error.';
+    unless($input =~ /^\w+$/) {
+        exit;
+    }
+    my $username = $input;
+
+    my $i;
+    for($i = 1; $i < 999; $i++) {
+        my $twit = &do_scrape_twit($username, $i);
+        unless($twit) { last; }
+        foreach(@$twit) {
+            print $_->{uri}."\t".$_->{date}."\t".
+                  Encode::encode('utf8', $_->{msg})."\n";
+        }
+    }
+    print "\nscraping: $i\n";
+
+    exit;
+
+sub do_scrape_twit {
+    my $id = shift or return;
+    my $page = shift or return;
+    my $uri = new URI('http://twitter.com/'.$id.'?page='.$page);
+    sleep(5);
+    return scraper {
+        process 'ol.statuses li span.status-body',
+            'twit[]' => scraper {
+                process 'span.entry-content', msg => 'TEXT';
+                process 'span a.entry-date', uri => '@href';
+                process 'span a.entry-date span.published', date => 'TEXT';
+            };
+        result 'twit';
+    }->scrape($uri);
+}
+
