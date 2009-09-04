7e972bc25770aa5800e5a4396c2003b067215325
kujoo <kujoo@wiir.jp>
2009-09-04 15:44:09 +0900

    date -> local, print-layout -> table, add icon-get

--- twitrss.pl
+++ twitrss.pl
@@ -15,50 +15,70 @@ use XML::TreePP;
 use HTML::Entities;
 use Web::Scraper;
 use URI;
+use DateTime::Format::HTTP;
 #use Data::Dumper;
 
     my $input = $ARGV[0] or die 'no input error.';
     unless($input =~ /^\w+$/) {
         exit;
     }
-    my $username = $input;
-    my $urirss = &get_uri_rss($username) or die('doesnot get user-code.');
+    my $un = $input;
+    my($urirss, $tw, $ic) = &get_rss_uri($un) or die('doesnot get user-code.');
 
     my $i;
     for($i = 1; $i < 999; $i++) {
-        my $twit = &do_scrape_twit_rss($urirss, $i);
+        my $twit = &get_rss_twit($urirss, $i);
         unless($twit) { last; }
+        print '<table>';
         foreach(@$twit) {
-            print $_->{pubDate}."\t".$_->{link}."\t".
-                  HTML::Entities::decode($_->{title})."\n";
+            my($dt, $ti) = &get_date_timestamp($_->{pubDate});
+            my $ps = HTML::Entities::decode($_->{title});
+            my $lk = $_->{link};
+            print<<"EOD";
+$dt
+<tr><td><a href="$tw"><img src="$ic" border="0" width="48"></a></td><td><a href="$tw">$un</a></td><td>$ti</td><td>$ps</td><td><a href="$lk">link</a></td></tr>
+EOD
         }
+        print '</table>';
     }
     print "\nscraping: $i\n";
 
     exit;
 
-sub get_uri_rss {
+sub get_rss_uri {
     my $id = shift or return;
-    my $uri = new URI('http://twitter.com/'.$id);
+    my $turi = 'http://twitter.com/'.$id;
+    my $uri = new URI($turi);
+    my $ico = scraper {
+        process 'div.profile-head div.listable h2 a img', 'ico' => '@src';
+        result 'ico';
+    }->scrape($uri);
     my $rss = scraper {
         process 'html head link', 'rss[]' => '@href';
         result 'rss';
     }->scrape($uri);
     foreach(@$rss) {
         if($_ =~ /^http:\/\/twitter\.com\/statuses\/user_timeline\/\d+\.rss$/) {
-            return $_;
+            return $_, $turi, $ico;
         }
     }
     return;
 }
 
-sub do_scrape_twit_rss {
+sub get_rss_twit {
     my $uri = shift or return;
     my $page = shift or return;
-    sleep(8);
+    sleep(7);
     my $xtpp = XML::TreePP->new() or return;
     my $rss = $xtpp->parsehttp(GET => $uri.'?page='.$page) or return;
     my $item = $rss->{rss}->{channel}->{item} or return;
     return $item;
+}
+
+sub get_date_timestamp {
+    my $timestamp = shift or return;
+    my $dt = DateTime::Format::HTTP->parse_datetime($timestamp)->set_time_zone('local');
+    my($date, $time) = split(/T/, $dt);
+    return $date, $time;
 }
 
